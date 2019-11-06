8cc7106f1f464e877e8a1cd75eeb6956dc1673f2
Robert te Kaat <hvkaat@aahg.onmicrosoft.com>
2019-11-06 13:48:31 +0100

    Customized Dockerfile

--- src/VotingApiBroken/buildAndPublish.sh
+++
@@ -1,6 +0,0 @@
-docker login -u kubedemo -p =O1CicSshvxwuVlEemfphqDF8u1LZzsr kubedemo.azurecr.io
-docker build . -t kubedemo.azurecr.io/kubedemo/votingapi:v2
-docker push kubedemo.azurecr.io/kubedemo/votingapi:v2
-
-
-
